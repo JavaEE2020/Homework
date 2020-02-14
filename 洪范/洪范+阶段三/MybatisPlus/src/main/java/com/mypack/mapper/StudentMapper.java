6330b00cfe70ebf322ab9b026fcbdacbdc3f7f03
H1245783609 <1245783609@qq.com>
2020-02-14 11:04:50 +0800

    Add files via upload

--- 洪范/洪范+阶段三/MybatisPlus/src/main/java/com/mypack/mapper/StudentMapper.java
+++
@@ -1,8 +0,0 @@
-package com.mypack.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.mypack.entity.Student;
-
-public interface StudentMapper extends BaseMapper<Student> {
-
-}
--- 洪范/洪范+阶段三/MybatisPlus/src/main/java/com/mypack/service/StudentService.java
+++
@@ -1,9 +0,0 @@
-package com.mypack.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.mypack.entity.Student;
-
-public interface StudentService extends IService<Student> {
-
-    void insert(Student student);
-}
